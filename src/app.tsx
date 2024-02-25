00a0f8a44b64d1f19b64dafd0e11b98505a9a533
RomanBykov <rmnbykov@gmail.com>
2024-02-25 14:48:18 +0300

    module3-task1: добавляет первый маршрут, страницу 404, css-модули для 404

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { AppRoute } from './common/const';
 import { Offer } from './common/types';
 import Main from './pages/main';
+import NotFound from './pages/not-found';
 
 type Props = {
   offers: Offer[];
 }
 
 const App = ({ offers }: Props) => (
-  <Main offers={offers} />
+  <BrowserRouter>
+    <Routes>
+      <Route path={AppRoute.Main} element={<Main offers={offers} />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/pages/not-found/not-found.tsx
@@ -0,0 +1,10 @@
+import style from './styles.module.css';
+
+const NotFound = () => (
+  <div className={style['not-found']}>
+    <h1>404 Not Found</h1>
+    <div>¯\_(ツ)_/¯</div>
+  </div>
+);
+
+export default NotFound;
