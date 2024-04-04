77704d1861721f8764231564afc267503e4d8c5b
RomanBykov <rmnbykov@gmail.com>
2024-04-04 21:52:07 +0300

    Дополняет логику приватного машрута

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -4,6 +4,7 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { AppRoute } from '@/common/const';
 import { TLocationFrom } from '@/common/types';
 import useAppSelector from '@/hooks/use-app-selector';
+import { getToken } from '@/services/token';
 import { selectUser } from '@/store/auth/selectors';
 
 type TProps = {
@@ -14,8 +15,9 @@ type TProps = {
 const ProtectedRoute = ({ isLoginPage, children }: TProps) => {
   const location: Location<TLocationFrom> = useLocation() as Location<TLocationFrom>;
   const user = useAppSelector(selectUser);
+  const token = getToken();
 
-  if (isLoginPage && user) {
+  if (token || isLoginPage && user) {
     const from: Location = location.state?.from || { pathname: AppRoute.Main } as Location;
 
     return <Navigate to={from} />;
